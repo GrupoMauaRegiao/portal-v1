0b3b28eb685691ff27ccd6de5dc6479c8fa24c44
Marcos Garcia <marckfree@gmail.com>
2013-03-26 16:53:01 -0300

    Eliminando elementos inúteis

--- agenda/agenda.php
+++ agenda/agenda.php
@@ -3,21 +3,7 @@
     /home/paginas/agenda.php 
 */
 ?>
-
-<table width="100%" border="0" cellpadding="0" cellspacing="0"> 
-
-  <tr>
-    <td>
-      <img src="/images/setona.jpg" width="30" height="30" />
-      <div>Agenda de Eventos</div>
-    </td>
-  </tr>
-
-  <tr>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>
+<div id="agenda">
 <?php
   $largura = 100;
   $limite = 10;
@@ -31,10 +17,6 @@
   $Cor1 = "#003399";
   $Cor2 = "#000000";
   $acao = "listar";
-
   include "agendas/exibe.php";
 ?>
-    </td>
-  </tr>
-
-</table>
+</div>
