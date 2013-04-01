1f80b530a27b20f6423865e102ce7908bb64e39a
Marcos Garcia <marckfree@gmail.com>
2013-04-01 16:23:15 -0300

    Adicionando busca.php - eliminando tabelas do layout

---
+++ videos/busca.php
@@ -0,0 +1,64 @@
+<?php
+
+$variables=(strtolower($_SERVER['REQUEST_METHOD'])== 'GET') ? $_GET : $_POST;
+
+foreach ($variables as $k=> $v)
+  $$k=$v;
+
+$palavra = "Vídeos";
+$tabela1 = "tb_videos";
+$tabela2 = $tabela1."_categorias";
+$file = "video";
+$form1 = "BUSCA"; 
+
+if ($FormatoForm == "H") { ?>
+
+<form action="/videos" method="POST" name="BUSCA">
+  <input name="pg" type="hidden" value="ver_video" />
+  <input name="acao" type="hidden" value="ver2" />
+  <input name="page" type="hidden" value="" />
+  <input name="key" type="text" />
+
+  <select name="id_categoria">
+    <option value="">Categoria</option>
+<?php
+
+$sql5 = mysql_query("SELECT * FROM $tabela2 WHERE status='S' order by nome");
+
+while ($dados5 = mysql_fetch_array($sql5)) { ?>
+    <option value='<?=$dados5[id]?>' <?=($id_categoria == $dados5[id]) ? "selected" : "";?>>
+      <?=$dados5[nome]?>
+    </option>
+<?php }?>
+  </select>
+  <input type="submit" class="input" value="Pesquisar" />
+</form>
+
+<?php }
+
+if ($FormatoForm == "V") { ?>
+
+<form action="/videos" method="POST" name="<?=$form1?>">
+  <input name="pg" type="hidden" value="ver_video" />
+  <input name="acao" type="hidden" value="ver2" />
+  <input name="page" type="hidden" value="<?=$_GET[page]?>" />
+  <input name="key" type="text" />
+  <select name="id_categoria" style="width:220px;"  class="input" onblur="this.className='input';" onfocus="this.className='inputon';" >
+    <option value="">Categoria</option>
+
+<?php
+
+$sql5 = mysql_query("SELECT * FROM $tabela2 WHERE status='S' order by nome");
+
+while ($dados5 = mysql_fetch_array($sql5)) { ?>
+  <option value='<?=$dados5[id]?>' <?=($id_categoria==$dados5[id]) ? "selected" : "";?>>
+    <?=$dados5[nome]?>
+  </option>
+
+<?php } ?>
+  </select>
+
+  <input type="submit" value="Pesquisar" />
+</form>
+
+<?php } ?>
