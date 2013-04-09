941d0075c86b178018eb486899d92588c7b2b47a
Marcos Garcia <marckfree@gmail.com>
2013-04-09 10:28:37 -0300

    Implementando popup na página inicial.

--- enviar-email.php
+++ enviar-email.php
@@ -6,21 +6,19 @@
   }
 
   $destinatario = "arte@revistamaua.com.br";
-  $assunto = "Mensagem: Fale Conosco do Portal";
-  $nome = $_POST["campo-nome"];
-  $email = $_POST["campo-email"];
-  $mensagem = $_POST["campo-mensagem"];
+  $assunto = "COMPRA MAUÁ E REGIÃO - Inscrição";
+  $nome = $_POST["nome"];
+  $email = $_POST["email"];
   $conteudo = 
     '<p><b>Nome:</b> ' . $nome . '</p>
      <p><b>E-mail:</b> ' . $email . '</p>
-     <p><b>Assunto:</b> ' . $assunto . '</p>
-     <p><b>Mensagem:</b><pre> ' . $mensagem . '</pre></p>';
-
+     <p><b>Assunto:</b> ' . $assunto . '</p>';
+     
   $headers .= "MIME-Version: 1.1" . $quebraLinha;
   $headers .= "Content-type: text/html; charset=utf-8" . $quebraLinha;
   $headers .= "From: " . $email . $quebraLinha;
 
-  if(!mail($destinatario, $assunto, $conteudo, $headers , "-r" . $destinatario)) {
+  if (!mail($destinatario, $assunto, $conteudo, $headers , "-r" . $destinatario)) {
     mail($destinatario, $assunto, $conteudo, $headers);
   }
 ?>
