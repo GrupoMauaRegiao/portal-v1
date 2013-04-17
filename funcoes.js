a22624121f4ce23641ea9b3f8372a4282c774161
Marcos Garcia <marckfree@gmail.com>
2013-04-17 15:50:16 -0300

    Corrigindo cor e removendo identificação de browser desnecessária

--- funcoes.js
+++ funcoes.js
@@ -106,9 +106,7 @@ jQuery(document).ready(function () {
   // Browser
   jQuery.browser = {};
   jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase());
-  jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
   jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
-  jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
 
   if (jQuery.browser.mozilla) {
     jQuery('.campo-busca').css({
