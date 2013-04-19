f32f43fa7c1263391538cc96abe3a57725e96eb8
Marcos Garcia <marckfree@gmail.com>
2013-04-19 18:04:05 -0300

    Melhorando indentação e alterando nome do evento de 'event' para 'evt'

--- teste.php
+++ teste.php
@@ -1,55 +1,51 @@
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato" />
-<link rel="stylesheet" href="http://portalmauaeregiao.com.br/paginas/css/tdefaut.css" type="text/css" media="all" />
-<!--[if IE]><link rel="stylesheet" href="http://portalmauaeregiao.com.br/paginas/css/tdefaut-ie.css" type="text/css" media="all" /><![endif]-->
-
- <script type="text/javascript" src="http://portalmauaeregiao.com.br/paginas/js/jquery.js"></script>
-
- <script type="text/javascript" src="http://portalmauaeregiao.com.br/paginas/js/superfish.js"></script>
- <script type="text/javascript" src="http://portalmauaeregiao.com.br/paginas/js/nivo.slider.js"></script>
- <script type="text/javascript" src="http://portalmauaeregiao.com.br/paginas/js/nivo-configs.js"></script>
- 
- <script type="text/javascript">
+<link rel="stylesheet" href="http://portalmauaeregiao.com.br/paginas/css/tdefaut.css" />
+<!--[if IE]>
+  <link rel="stylesheet" href="http://portalmauaeregiao.com.br/paginas/css/tdefaut-ie.css" />
+<![endif]-->
+ <script src="http://portalmauaeregiao.com.br/paginas/js/jquery.js"></script>
+ <script src="http://portalmauaeregiao.com.br/paginas/js/superfish.js"></script>
+ <script src="http://portalmauaeregiao.com.br/paginas/js/nivo.slider.js"></script>
+ <script src="http://portalmauaeregiao.com.br/paginas/js/nivo-configs.js"></script>
+ <script>
   var $ = jQuery.noConflict();
-  
   $(document).ready(function (){
     $('#slider').nivoSlider({
-      effect:"sliceDownLeft",
-      slices:20,
-      boxCols:1,
-      boxRows:1,
-      animSpeed:100,
-      pauseTime:5000,
-      startSlide:4,
-      directionNav:true,
-      directionNavHide:false,
-      controlNav:true,
-      controlNavThumbs:false,
-      pauseOnHover:false,
-      manualAdvance:false
+      effect: "sliceDownLeft",
+      slices: 20,
+      boxCols: 1,
+      boxRows: 1,
+      animSpeed: 100,
+      pauseTime: 5000,
+      startSlide: 4,
+      directionNav: true,
+      directionNavHide: false,
+      controlNav: true,
+      controlNavThumbs: false,
+      pauseOnHover: false,
+      manualAdvance: false
     });
 
-
-    var $slider
+    var $slider;
     $slider = $('#slider');
 
-    $slider.on('mouseover', function(event) {
+    $slider.on('mouseover', function(evt) {
       $('.nivo-directionNav a').fadeIn(200);
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      event.stopPropagation();
+      evt.preventDefault();
+      evt.stopImmediatePropagation();
+      evt.stopPropagation();
     });
 
-    $slider.on('mouseleave', function(event) {
+    $slider.on('mouseleave', function(evt) {
       $('.nivo-directionNav a').fadeOut(200);
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      event.stopPropagation();
+      evt.preventDefault();
+      evt.stopImmediatePropagation();
+      evt.stopPropagation();
     });
-
   });
  </script>
 
- <?php
-include "CriarBannerDestaque.php";
-echo criarBannerDestaque();
+<?php
+  include "CriarBannerDestaque.php";
+  echo criarBannerDestaque();
 ?>
