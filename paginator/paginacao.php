fa494febddb6d605746ef687da189138fbb36c4b
Marcos Garcia <marckfree@gmail.com>
2013-04-30 17:27:08 -0300

    Refatorado

--- paginator/paginacao.php
+++ paginator/paginacao.php
@@ -1,59 +1,26 @@
-
-
-<table border="0" align="right" cellpadding="0" cellspacing="0">
-
-
-
+<table>
   <tr>
+    <td>
+      <p>&nbsp;  </p>
+      <table border="0" cellpadding="2" cellspacing="1">
+        <tr>
+          <?php
+            for ($i = 1; $i < $page; $i += 1) {
+              if ($i >= $page-5) {
+                echo "<td width='12' align='center' style='border:1px solid $Cor1'><a href='?page=$i'><b><font color='#1E4B7A'>$i</font></b></a></td>";
+              }
+            }
 
-    <td align="center"><p>&nbsp;  </p>
-
-	
-
-	
-
-<table border="0" cellpadding="2" cellspacing="1">
-
-<tr>
-
-	<? 
-
-
-
-
-
-for($i=1; $i<$page; $i++)
-
-if($i>=$page-5)
-
-
-
-echo "<td width='12' align='center' style='border:1px solid $Cor1'><a href='?page=$i'><b><font color='#1E4B7A'>$i</font></b></a></td>";
-
-echo "<td width='12' align='center' style='border:1px solid $Cor1; color:#FFF' bgcolor='#1E4B7A'><b>$page</b></td>";
-
-
-
-for($i=$page+1; $i<=$tp; $i++)
-
-if($i<=$page+5)
-
-
-
-echo "<td width='12' align='center' style='border:1px solid $Cor1;'><a href='?page=$i'><b><font color='#1E4B7A'>$i</font></b></a></td>";
-
-
-
-?>
-
-</tr>
-
-</table>
-
-
-
-</td>
-
-</tr>
-
+            echo "<td width='12' align='center' style='border:1px solid $Cor1; color:#FFF' bgcolor='#1E4B7A'><b>$page</b></td>";
+          
+            for ($i = $page + 1; $i <= $tp; $i += 1) {
+              if ($i <= $page + 5) {
+                echo "<td width='12' align='center' style='border:1px solid $Cor1;'><a href='?page=$i'><b><font color='#1E4B7A'>$i</font></b></a></td>";
+              }
+            }
+          ?>
+        </tr>
+      </table>
+    </td>
+  </tr>
 </table>
